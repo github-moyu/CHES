e3f7d7bf648118c3db3bfb53be56f17de2677292
github-moyu <10744826+github-moyu@users.noreply.github.com>
2020-12-26 06:17:41 -0500

    Update and rename !Figure3h3h4h5RiskChg_20200625.R to figure3.R

--- MangroveHurrianeImpactGlobal/figures/figure3.R
+++ MangroveHurrianeImpactGlobal/figures/figure3.R
@@ -1,5 +1,3 @@
-###
-#############
 rm(list = ls())
 
 wd <- paste0("C:/Users/Y/Desktop/CHES/hurricane/")
@@ -36,9 +34,6 @@ layout(matrix(c(1,2), 1, 2, byrow = TRUE),widths=c(1,2))
   
 par(lwd=1)
 bp <-barplot(as.matrix(cb[,c('hii')]),col=adjustcolor(cols,alpha.f = 0.2),space=0.6,border=cols,xlab="", ylab="", yaxt="n",xaxt="n")
-#text(1.1,0.15,paste0(round(cb[which(cb$cat=='h3'),'hii']/cb[which(cb$cat=='h3'),'hii'],digits = 1)),cex=notecex,col=cols[1],pos=1)
-#text(1.1,0.32,paste0(round(cb[which(cb$cat=='h4'),'hii']/cb[which(cb$cat=='h3'),'hii'],digits = 1)),cex=notecex,col=cols[2],pos=1)
-#text(1.1,0.73,paste0(round(cb[which(cb$cat=='h5'),'hii']/cb[which(cb$cat=='h3'),'hii'],digits = 1)),cex=notecex,col=cols[3],pos=1)
 text(1.1,0.15,'1.0',cex=0.5,col=cols[1],pos=1)
 text(1.1,0.32,'1.3',cex=0.5,col=cols[2],pos=1)
 text(1.1,0.73,'4.0',cex=0.5,col=cols[3],pos=1)
@@ -58,7 +53,6 @@ axis(4, at=seq(0,sum(cb$Ftr),sum(cb$Ftr)/2),lwd=0, labels=seq(0,100,50), las=1, 
        col.axis="darkred")
 mtext("Percentage (%)", side=4, line = 0.7,cex=0.5,las=3,col="darkred")
   
-#axis(1, at=seq(1,2,1),labels= seq(1,2,1))
 axis(1, at=bp,lwd=0, labels=c("Present HRI","End-of-century HRI"), las=1,cex.axis=0.5,line=-1.2)
   
 text(bp[1],0.2*sum(cb$Ftr),paste0(round(cb$Nw_pct[1],digits = 2)*100,'%'),cex=0.5,pos=1)
@@ -76,29 +70,3 @@ text(mean(bp),1*sum(cb$Ftr),paste0('Tot.: +',round(cb$pct[4],digits = 2)*100,'%'
   
 dev.off()
 }
-
-
-if (FALSE){ #scatter plot
-  tiff(file = paste0(wd,'result/graph/','impacth3h4h5.tif'), width = 89, height = 60, units = 'mm', res=300) 
-  par(mfrow=c(1,1),oma=c(1,2,0.5,2),mar=c(1,1,0,1)) 
-  
-  plot(fre_x,imp_y,
-       xlim=c(-0.2,1.2),ylim=c(-0.2,1.2),
-       xaxt='n',yaxt='n',xlab='',ylab='',bty="n",
-       cex=sqrt(impfull_size)*10,pch=21, 
-       col=cols,bg=adjustcolor(cols,alpha.f = 0.2))
-  text(fre_x,imp_y,labels=c('h5','h4','h3'),cex=0.7,font=2,col=cols)
-  
-  axis(1,at=c(0,0.5,1),labels=FALSE,tck=-0.03,col='black',col.ticks='black', col.axis='black')
-  axis(1,at=c(0,0.5,1),labels = c(0,0.5,1),line=-0.8,lwd=0,cex.axis=0.55)
-  mtext('Impact', side=1, line=0.8, col='black', cex=0.55)
-  
-  axis(2,at=c(0,0.5,1),labels=FALSE,tck=-0.03,col='black',col.ticks='black', col.axis='black')
-  axis(2,at=c(0,0.5,1),labels = c(0,0.5,1),line=-0.6,lwd=0,cex.axis=0.55)
-  mtext('Frequency', side=2, line=1.1, col='black', cex=0.55)
-  
-  dev.off()}
-
-plot(1,1,cex=sqrt(10))
-points(1,1,cex=sqrt(20))
-##############
